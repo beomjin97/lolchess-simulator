1d4465fc9ac35e05a1deaa8faa116f1bba6b4afe
beomjin97 <qjawls646@gmail.com>
2022-03-05 16:57:54 +0900

    multi select

--- src/components/ChampionBox/ChampionBox.tsx
+++ src/components/ChampionBox/ChampionBox.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./ChampionBox.module.scss";
 import { useRecoilState } from "recoil";
-import activatedState from "../../store/store";
+import activeState from "../../store/store";
 
 interface propsType {
   name: string;
@@ -10,16 +10,24 @@ interface propsType {
 }
 
 export default function ChampionBox({ name, url, cost }: propsType) {
-  const [activated, setActivated] = useRecoilState(activatedState);
+  // const [active, setActive] = useState("");
+  const [globalActive, setGlobalActive] = useRecoilState(activeState);
 
-  function handleClick(): void {
-    setActivated((prev) => !prev);
+  function handleClick(e: any): void {
+    if (globalActive.includes(e.target.alt)) {
+      setGlobalActive((prev) => prev.filter((item) => item !== e.target.alt));
+    } else {
+      setGlobalActive((prev) => [...prev, e.target.alt]);
+      // alert(globalActive);
+    }
   }
 
   return (
     <div className={`${styles["combined"]} ${styles[`combined${cost}`]}`}>
       <div
-        className={`${styles["box"]} ${activated && styles["active"]}`}
+        className={`${styles["box"]} ${
+          globalActive.includes(name) && styles["active"]
+        }`}
         onClick={handleClick}
       >
         <img src={url} alt={name} className={styles["img"]} />
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,8 @@
 import { atom } from "recoil";
 
-const activatedState = atom({
-  key: "actvatedState", // unique ID (with respect to other atoms/selectors)
-  default: false, // default value (aka initial value)
+const activeState = atom({
+  key: "activeState", // unique ID (with respect to other atoms/selectors)
+  default: [""], // default value (aka initial value)
 });
 
-export default activatedState;
+export default activeState;
